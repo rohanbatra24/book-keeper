dda19c2d70d747ab52b4ff76b913f5d35c623116
Rohan Batra <rohanbatra24@gmail.com>
2021-05-25 21:50:13 -0400

    javascript for modal

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarkcontainer = document.getElementById("bookmarks-container");
 
+// SHow Modal, focus on input
+const showModal = (params) => {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+};
+
+// / Modal event listeners
+modalShow.addEventListener("click", showModal);
